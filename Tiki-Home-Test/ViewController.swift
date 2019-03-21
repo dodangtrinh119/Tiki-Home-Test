bf047ef6f02915f32bed8c51ee47ca76175c0fe6
Trinh Do <dodangtrinh119@gmail.com>
2019-03-21 17:02:39 +0700

    Initial Commit

---
+++ Tiki-Home-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiki-Home-Test
+//
+//  Created by Đăng Trình on 3/21/19.
+//  Copyright © 2019 Dang Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
